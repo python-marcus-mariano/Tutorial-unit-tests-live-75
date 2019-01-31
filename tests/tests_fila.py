2e595c0190ed74a996067b11688022515424ae4b
mmariano <marcusney@gmail.com>
2019-01-30 21:58:40 -0300

    adicinando Fixture do método - Método setup

--- tests/tests_fila.py
+++ tests/tests_fila.py
@@ -16,36 +16,30 @@ from app.fila import Fila
 class TestFila(TestCase):
     """TestCase."""  
 
+    def setUp(self):
+        """Fixture do método - Método setup """
+        self.fila = Fila()
+
     def test_quando_5_entrar_na_fila_5_deve_estar_no_final_da_fila(self):
         """Método testa quando 5 esta no final da fila"""
         entrada = 5
         saida_esperada = 5
-
-        fila = Fila()
-
-        # quando 5 entrar na fila  
-        fila.entrar(entrada)
         
-        # erro 
-        # fila.entrar(10)
+        # quando 5 entrar na fila  
+        self.fila.entrar(entrada)
 
         # então 5 deve estar na fila
         # assertin para verificar se esta dentro
-        self.assertEqual(saida_esperada, fila[-1])
+        self.assertEqual(saida_esperada, self.fila[-1])
     
     def test_quando_10_entrar_na_fila_10_deve_estar_no_final_da_fila(self):
         """Método testa quando 10 esta no final da fila"""
         entrada = 10    
         saida_esperada = 10 
 
-        fila = Fila()
-
         # quando 10 entrar na fila  
-        fila.entrar(entrada)
-        
-        # erro 
-        # fila.entrar(20)
+        self.fila.entrar(entrada)        
 
         # então 10 deve estar na fila
         # assertin para verificar se esta dentro
-        self.assertEqual(saida_esperada, fila[-1])
+        self.assertEqual(saida_esperada, self.fila[-1])
