1a5ed9f6c525699b043740c8914c50d5b6762fb9
mmariano <marcusney@gmail.com>
2019-01-30 22:15:43 -0300

    adicinando Fixture do método - Método tearDown

--- tests/tests_fila.py
+++ tests/tests_fila.py
@@ -19,6 +19,11 @@ class TestFila(TestCase):
     def setUp(self):
         """Fixture do método - Método setup """
         self.fila = Fila()
+        print('setup')
+
+    def tearDown(self):
+        """Fixture do método - Método tearDown """
+        print('tearDown')
 
     def test_quando_5_entrar_na_fila_5_deve_estar_no_final_da_fila(self):
         """Método testa quando 5 esta no final da fila"""
@@ -31,6 +36,7 @@ class TestFila(TestCase):
         # então 5 deve estar na fila
         # assertin para verificar se esta dentro
         self.assertEqual(saida_esperada, self.fila[-1])
+        print(self.fila)
     
     def test_quando_10_entrar_na_fila_10_deve_estar_no_final_da_fila(self):
         """Método testa quando 10 esta no final da fila"""
@@ -43,3 +49,4 @@ class TestFila(TestCase):
         # então 10 deve estar na fila
         # assertin para verificar se esta dentro
         self.assertEqual(saida_esperada, self.fila[-1])
+        print(self.fila)
